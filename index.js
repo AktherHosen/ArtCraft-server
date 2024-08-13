6ddb590ff0058815baf6fe9489153ac3dc80c3ce
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-08-13 11:54:35 +0600

    delete functionality added

--- index.js
+++ index.js
@@ -77,6 +77,15 @@ async function run() {
       const result = await craftCollection.updateOne(filter, craft, options);
       res.send(result);
     });
+
+    //delete
+    app.delete("/deleteCraft/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
